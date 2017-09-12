f8f41cb1d007c52bf3b79efa4f614532e5cda57f
Otlet <otlet@protonmail.com>
2017-09-12 15:35:22 +0200

    CHANGE: Change displaying a streams

--- static/js/stream.js
+++ static/js/stream.js
@@ -3,18 +3,12 @@ var Stream = {
     start: function (url, urlEsport, mixer_url, twitch_url) {
         twitch_url = twitch_url.replace("_/", '');
         mixer_url = mixer_url.replace("_/", '');
-        this.template = '<a class="carousel-item hoverable" href="' + twitch_url + '{{ 1 }}">' +
-            '<img class="responsive-img" src="{{ 3 }}" alt="{{ 0 }}"></a>';
-        this.templateESport = '<div class="col s12 m3">' + '<div class="card hoverable">' +
+        this.template = '<div class="col s12 m3">' + '<div class="card hoverable">' +
             '<div class="card-image">' + '<a href="' + twitch_url + '{{ 1 }}">' +
             '<img src="{{ 3 }}">' +
             '<span class="card-title hide-on-med-and-down">' +
             '<span class="new badge" data-badge-caption="na Twitch">{{ 0 }}</span>' +
             '</span></a></div></div></div>';
-            // '<a class="carousel-item hoverable" href="' + twitch_url + '{{ 0 }}">' +
-            // '<img class="responsive-img" src="{{ 3 }}" alt="{{ 0 }}"></a>';
-
-        $('.carousel.carousel-slider').carousel({fullWidth: true});
 
         $.get(url, function (data, status) {
             Stream.generate(data, mixer_url, twitch_url);
@@ -26,16 +20,16 @@ var Stream = {
     },
     generate: function (data, mixer_url, twitch_url) {
         for (var i = 0; i < data.length; i++) {
-            $(".stream-container .carousel").append(Mustache.render(this.template, data[i]));
-            console.log(data[i]);
+            $(".stream-container-row").append(Mustache.render(this.template, data[i]));
         }
-
-        $('.stream-container .carousel').carousel();
     },
     generateESport: function (data, mixer_url, twitch_url) {
-        for (var i = 0; i < data.length; i++) {
-            $(".esport-stream-container .esport-stream-row").append(Mustache.render(this.templateESport, data[i]));
-            console.log(data[i]);
+        if (data.length !== 0) {
+            for (var i = 0; i < data.length; i++) {
+                $(".esport-stream-row").append(Mustache.render(this.template, data[i]));
+            }
+        } else {
+            $(".esport-stream-row").remove();
         }
     }
 };
