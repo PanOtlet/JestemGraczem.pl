d7092199988eab3c98fb089503ef228496dff27d
Paweł Otlewski <otlet@otlet.pl>
2018-02-10 23:15:51 +0100

    Update urls.py

--- stream/urls.py
+++ stream/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, register_converter
 from django.views.decorators.cache import cache_page
 from . import views, url_converters
 
-register_converter(url_converters.Username, 'wacek')
+#register_converter(url_converters.Username, 'wacek')
 
 urlpatterns = [
-    path('mixer/<wacek:username>/', views.mixer, name='stream.mixer'),
+    #path('mixer/<wacek:username>/', views.mixer, name='stream.mixer'),
     path('twitch/<slug:username>/', views.twitch, name='stream.twitch'),
     path('live/', cache_page(60 * 1)(views.stream_api), name='stream.live'),
     path('live/esport', cache_page(60 * 10)(views.esport_stream_api), name='stream.live.esport'),
