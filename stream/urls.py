cb718e4a8a56397276e533a87107700d6bed800b
otlet <otlet@otlet.pl>
2018-02-20 13:49:55 +0100

    Cleanup

--- stream/urls.py
+++ stream/urls.py
@@ -7,6 +7,5 @@ register_converter(url_converters.Username, 'wacek')
 urlpatterns = [
     path('live/', cache_page(60 * 1)(views.stream_api), name='stream.live'),
     path('live/esport', cache_page(60 * 10)(views.esport_stream_api), name='stream.live.esport'),
-    path('<str:username>/', views.streamer, name='stream.streamer'),
     path('', views.index, name='stream.index'),
 ]
