1fdb70f0fd714577e230f9258fc5036811a7e797
otlet <otlet@otlet.pl>
2018-02-22 21:29:38 +0100

    FIX: robots and humans

--- service/urls.py
+++ service/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.views.decorators.cache import cache_page
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
@@ -9,5 +11,7 @@ urlpatterns = [
     path('livestreams/', cache_page(60 * 1)(views.livestreams), name='livestreams'),
     path('youtube/', cache_page(60 * 1)(views.youtube), name='youtube'),
     path('gameservers/', cache_page(60 * 1)(views.gameservers), name='gameservers'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
+    path('humans.txt', TemplateView.as_view(template_name="humans.txt", content_type="text/plain"), name="humans"),
     path('', cache_page(60 * 1)(views.index), name='index'),
 ]
